60cc57c580dea601b0fb98f55073a79e2220a08d
twixour <twixour@gmail.com>
2023-07-21 06:44:15 +0530

    Initial Commit

---
+++ PDFGenerator/PDFGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFGeneratorApp.swift
+//  PDFGenerator
+//
+//  Created by Raushan Kashyap on 21/07/23.
+//
+
+import SwiftUI
+
+@main
+struct PDFGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
